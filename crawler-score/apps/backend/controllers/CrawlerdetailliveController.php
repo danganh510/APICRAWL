4eefb894421a07b4208a4b109708db82d43ec33b
danganh <danganh.hna@gmail.com>
2023-03-01 18:28:09 +0700

    fix small

--- crawler-score/apps/backend/controllers/CrawlerdetailliveController.php
+++ crawler-score/apps/backend/controllers/CrawlerdetailliveController.php
@@ -18,7 +18,7 @@ class CrawlerdetailliveController extends ControllerBase
     public function indexAction()
     {
         ini_set('max_execution_time', 20);
-        $start = microtime(true);
+        $start_time_cron = time() ;
         $is_live =  $this->request->get("isLive");
         $this->type_crawl = $this->request->get("type");
         $urlDetail = "https://www.flashscore.com/match/G67uHGC6/#/match-summary/match-summary";
@@ -26,10 +26,7 @@ class CrawlerdetailliveController extends ControllerBase
         //tab: info,tracker,statistics
         $crawler = new CrawlerDetail($this->type_crawl,$urlDetail, $is_live);
         $detail = $crawler->getInstance();
-        var_dump($detail,microtime(true) - $start);
-        exit;
-     
-        foreach ($dataResult as $id =>  $info) {
+        foreach ($detail as $id =>  $info) {
             $infoModel = new ScMatchInfo();
             $infoModel->setInfoMatchId($id);
             $infoModel->setInfoTime($info['info']);
@@ -37,6 +34,7 @@ class CrawlerdetailliveController extends ControllerBase
             $infoModel->setInfoSummary($info['tracker']);
             $infoModel->save();
         }
-        
+        echo "---finish in " . (time() - $start_time_cron) . " second";
+        die();
     }
 }
