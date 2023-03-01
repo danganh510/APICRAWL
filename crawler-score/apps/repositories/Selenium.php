b84d5619a5833f3599d6389c21d715a4f83876f9
dang Anh <danganh@bincg.com>
2023-03-01 11:01:02 +0700

    fix small

--- crawler-score/apps/repositories/Selenium.php
+++ crawler-score/apps/repositories/Selenium.php
@@ -38,7 +38,7 @@ class Selenium extends Component
     {
         $this->driver->get($url);
 
-        $this->driver->manage()->timeouts()->implicitlyWait(100); //to close tab
+      //  $this->driver->manage()->timeouts()->implicitlyWait(100); //to close tab
         //wait javascript load
         sleep(2);
         return $this->driver->getWindowHandle();
