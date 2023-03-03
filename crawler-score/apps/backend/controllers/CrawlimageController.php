78232a3bb79d03d8b2318bd15be3c9fc48001307
dang Anh <danganh@bincg.com>
2023-03-03 11:28:50 +0700

    Fix update logo

--- crawler-score/apps/backend/controllers/CrawlimageController.php
+++ crawler-score/apps/backend/controllers/CrawlimageController.php
@@ -11,16 +11,23 @@ class CrawlimageController extends ControllerBase
 
     public function indexAction()
     {
+        $start_time_cron = time() + 0 * 24 * 60 * 60;
+        echo "Start crawl logo team small in " . $this->my->formatDateTime($start_time_cron) . "/n/r";
         $arrTeam = ScTeam::findTeamLogoSmallNull(10);
         $crawlImage = new CrawlImage();
+        $total = 0;
         foreach ($arrTeam as $team) {
             $url_logo = "https://www.flashscore.com" . $team->getTeamLogo();
-            $result = $crawlImage->getImage($url_logo, Team::FOLDER_IMAGE_SMALL . "/" . $team->getTeamId());
-            var_dump($result);exit;
-            $team->setTeamLogoSmall($result['uploadFiles']);
-            $team->save();
+            $crawl = $crawlImage->getImage($url_logo, Team::FOLDER_IMAGE_SMALL . "/" . $team->getTeamId());
+            $team->setTeamLogoSmall($crawl['uploadFiles']);
+            $result = $team->save();
+            if ($crawl['status'] == "success") {
+                $total++;
+            }
         }
-        echo "succes";
+        echo "---total: " . $total;
+
+        echo "---finish in " . (time() - $start_time_cron) . " second";
         die();
     }
 }
