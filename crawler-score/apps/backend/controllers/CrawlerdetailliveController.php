e302f82f132d91fe1a971ff20f06d68874121cdf
danganh <danganh.hna@gmail.com>
2023-03-05 22:30:22 +0700

    fix

--- crawler-score/apps/backend/controllers/CrawlerdetailliveController.php
+++ crawler-score/apps/backend/controllers/CrawlerdetailliveController.php
@@ -114,6 +114,7 @@ class CrawlerdetailliveController extends ControllerBase
             $awayTeam->save();
         }
         $matchCrawl->save();
+        var_dump($matchCrawl->getMesages());exit;
         echo "---finish in " . (time() - $start_time_cron) . " second";
         die();
     }
