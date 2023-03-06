da07b011b7fed4a12036d4a603ea08290fa7dcfb
dang Anh <danganh@bincg.com>
2023-03-06 14:26:20 +0700

    fix small

--- crawler-score/apps/api/controllers/MatchController.php
+++ crawler-score/apps/api/controllers/MatchController.php
@@ -47,6 +47,7 @@ class MatchController extends ControllerBase
         if (!$arrMatch) {
             goto end;
         }
+        var_dump($arrMatch);exit;
         foreach ($arrMatch as $key => $match) {
             if (!is_array($match)) {
                 $match = (array) $match;
